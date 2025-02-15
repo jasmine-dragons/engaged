eb0d455219ecda79feffff5990f90c2f1bf8aa6e
Sean <ussa.say@gmail.com>
2025-02-15 12:26:19 -0800

    zoom sdk does not compile :pensive:
     ⨯ ReferenceError: self is not defined
        at [project]/src/app/start/page.tsx [app-ssr] (ecmascript) (src/app/start/page.tsx:3:0)
      1 | "use client";
      2 |
    > 3 | import ZoomMtgEmbedded, { EmbeddedClient } from "@zoom/meetingsdk/embedded";

--- client/src/app/start/page.tsx
+++ client/src/app/start/page.tsx
@@ -1,17 +1,26 @@
 "use client";
 
+import ZoomMtgEmbedded, { EmbeddedClient } from "@zoom/meetingsdk/embedded";
 import Link from "next/link";
-import styles from "./page.module.css";
 import { useSearchParams } from "next/navigation";
+import { useRef } from "react";
+import styles from "./page.module.css";
 
 export default function Start() {
   const searchParams = useSearchParams();
+
+  const ref = useRef<HTMLDivElement>(null);
+  const client = useRef<typeof EmbeddedClient>(null);
+  if (!client.current) {
+    client.current = ZoomMtgEmbedded.createClient();
+  }
 
   return (
     <div className={styles.container}>
       <h1 className={styles.heading}>Your experience is ready.</h1>
       <p>Click the link below to join your simulated classroom experience.</p>
       <pre>{searchParams.get("personalities")}</pre>
+      <div className={styles.zoomWrapper} ref={ref}></div>
       <Link href="#" className={styles.button}>
         Begin Class
       </Link>
