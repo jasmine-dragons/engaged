71e151422d0dac4c2d4cbca5de05c4e45ba6e6c6
Sean <ussa.say@gmail.com>
2025-02-15 05:43:29 -0800

    basic layout/styling of frontend

--- client/src/app/setup/page.tsx
+++ client/src/app/setup/page.tsx
@@ -1,37 +1,21 @@
 import Image from "next/image";
 import childImage from "@/../public/DEMO_CHILD.jpg";
-import child from "./page.module.css";
+import styles from "./page.module.css";
 
 export default function Setup() {
   return (
-    <form>
-      <h1>choose ur classroom</h1>
-      <div>
-        <button className={child.children}>
-          <Image
-            className={child.src}
-            width={40}
-            height={40}
-            src={childImage}
-            alt="child"
-          />
-          <Image
-            className={child.src}
-            width={40}
-            height={40}
-            src={childImage}
-            alt="child"
-          />
-          <Image
-            className={child.src}
-            width={40}
-            height={40}
-            src={childImage}
-            alt="child"
-          />
+    <div className={styles.container}>
+      <h1 className={styles.heading}>Set up your classroom.</h1>
+      <p>Choose from one of our classroom templates.</p>
+      <div></div>
+      <p>Or build your own class of students.</p>
+      <div></div>
+      <div className={styles.bottom}>
+        <p>1 of 4 students selected.</p>
+        <button className={styles.button} type="button">
+          Next
         </button>
       </div>
-      <button type="submit">next -&gt;</button>
-    </form>
+    </div>
   );
 }
