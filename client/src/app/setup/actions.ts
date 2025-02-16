f03118ec84970f5542dc18daf64af4f57e5cd19a
Sean <ussa.say@gmail.com>
2025-02-15 20:55:23 -0800

    switch from api back to temp delay for now

--- client/src/app/setup/actions.ts
+++ client/src/app/setup/actions.ts
@@ -4,11 +4,11 @@ import { createPracticeSession } from "@/lib/api";
 import { redirect } from "next/navigation";
 
 export async function startSession(personalities: string[]) {
-  // await new Promise((resolve) => setTimeout(resolve, 1000));
-  await createPracticeSession({
-    number_of_students: personalities.length,
-    student_avatar_types: personalities,
-  });
+  await new Promise((resolve) => setTimeout(resolve, 500));
+  // await createPracticeSession({
+  //   number_of_students: personalities.length,
+  //   student_avatar_types: personalities,
+  // });
   redirect(
     "/start?" + new URLSearchParams({ personalities: personalities.join(",") })
   );
