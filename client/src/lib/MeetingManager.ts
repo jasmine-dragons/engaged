f238624b792ddf2e8588570f558e20946d9c6b08
Sean <ussa.say@gmail.com>
2025-02-16 05:17:08 -0800

    feat: encode each chunk to a standalone wav file

--- client/src/lib/MeetingManager.ts
+++ client/src/lib/MeetingManager.ts
@@ -1,3 +1,5 @@
+import { encodeWav } from "./wav-encoder";
+
 /** milliseconds between sending chunks of audio */
 const AUDIO_CHUNK_PERIOD = 3000;
 
@@ -51,28 +53,46 @@ export async function makeManager(
   screenPreview.srcObject = screenStream;
   screenPreview.play();
 
-  const audioRecorder = new MediaRecorder(audioStream);
-  audioRecorder.ondataavailable = (event) => {
-    if (event.data.size > 0) {
-      // Check WebSocket state before sending
-      if (ws.readyState === WebSocket.OPEN) {
-        ws.send(event.data);
-      } else if (ws.readyState === WebSocket.CONNECTING) {
-        console.warn("WebSocket still connecting, data not sent");
-      } else if (ws.readyState === WebSocket.CLOSING) {
-        console.warn("WebSocket is closing, data not sent");
-      } else if (ws.readyState === WebSocket.CLOSED) {
-        console.error("WebSocket is closed, data not sent");
-        // Optionally attempt to reconnect here
-      }
+  let buffers: Float32Array[] = [];
+  const mediaStreamSource = audioContext.createMediaStreamSource(audioStream);
+  const scriptProcessorNode = audioContext.createScriptProcessor(8192, 2, 2);
+  scriptProcessorNode.addEventListener("audioprocess", (processEvent) => {
+    buffers.push(new Float32Array(processEvent.inputBuffer.getChannelData(0)));
+  });
+  mediaStreamSource.connect(scriptProcessorNode);
+  scriptProcessorNode.connect(audioContext.destination);
+
+  // const audioRecorder = new MediaRecorder(audioStream);
+  // audioRecorder.ondataavailable = (event) => {
+  //   if (event.data.size > 0) {
+  //     // Check WebSocket state before sending
+  //     if (ws.readyState === WebSocket.OPEN) {
+  //       ws.send(event.data);
+  //     } else if (ws.readyState === WebSocket.CONNECTING) {
+  //       console.warn("WebSocket still connecting, data not sent");
+  //     } else if (ws.readyState === WebSocket.CLOSING) {
+  //       console.warn("WebSocket is closing, data not sent");
+  //     } else if (ws.readyState === WebSocket.CLOSED) {
+  //       console.error("WebSocket is closed, data not sent");
+  //       // Optionally attempt to reconnect here
+  //     }
+  //   }
+  // };
+  // audioRecorder.start(AUDIO_CHUNK_PERIOD); // Send audio every PERIOD ms
+
+  let intervalId = setInterval(() => {
+    if (ws.readyState === WebSocket.OPEN) {
+      const blob = encodeWav(buffers, audioContext.sampleRate);
+      ws.send(blob);
     }
-  };
-  audioRecorder.start(AUDIO_CHUNK_PERIOD); // Send audio every PERIOD ms
+    buffers = [];
+  }, AUDIO_CHUNK_PERIOD);
 
   return {
     kill: () => {
       ws.close();
-      audioRecorder.stop();
+      // audioRecorder.stop();
+      clearInterval(intervalId);
       killStream(audioStream);
       killStream(videoStream);
       if (screenStream) {
